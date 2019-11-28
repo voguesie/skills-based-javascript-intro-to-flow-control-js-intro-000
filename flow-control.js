1e1d64eb1951e8c70077f3f903fde4f8ec1bf618
Michael Vogel <voguesie@alumni.stanford.edu>
2019-11-28 01:08:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,13 @@ if (age >= 13 && age <= 19) {
 }
 
 function ageChecker(age) {
-
+if (age < 13) {
+  return "You are a kid"
+} else if (age <= 19) {
+  return "You are a teenager!"
+} else {
+  return "You are a grownup"
+}
 }
 
 function ternaryTeenager(age) {
